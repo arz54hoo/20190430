ea77f13c58935ae23f282f011a849321e09d2596
seik <arz54hoo@naver.com>
2019-04-30 13:45:36 +0900

    ver 1.0

---
+++ cmake-build-debug/CMakeFiles/20190430.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/20190430.dir/main.cpp.obj"
+  "20190430.pdb"
+  "20190430.exe"
+  "20190430.exe.manifest"
+  "lib20190430.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/20190430.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
